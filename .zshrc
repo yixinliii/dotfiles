879c21c54bb1e66c985a2c70782a82f03b82a4d5
Yixin Li <yixinli@stanford.edu>
2024-05-06 06:20:33 -0700

    add hostname check for zsh

--- .zshrc
+++ .zshrc
@@ -99,22 +99,22 @@ source $ZSH/oh-my-zsh.sh
 # Example aliases
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
-alias qt="~/Qt/Tools/QtCreator/bin/qtcreator"
-alias vim="nvim"
+alias v="nvim"
 alias sc="ssh yixinli@sc.stanford.edu"
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/home/yixinliii/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/home/yixinliii/miniconda3/etc/profile.d/conda.sh" ]; then
-        . "/home/yixinliii/miniconda3/etc/profile.d/conda.sh"
+if [[ "$(hostname)" == "Kaiweis-MBP" ]]; then
+    # >>> conda initialize >>>
+    # !! Contents within this block are managed by 'conda init' !!
+    __conda_setup="$('/Users/yixinliii/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+    if [ $? -eq 0 ]; then
+        eval "$__conda_setup"
     else
-        export PATH="/home/yixinliii/miniconda3/bin:$PATH"
+        if [ -f "/Users/yixinliii/miniconda3/etc/profile.d/conda.sh" ]; then
+            . "/Users/yixinliii/miniconda3/etc/profile.d/conda.sh"
+        else
+            export PATH="/Users/yixinliii/miniconda3/bin:$PATH"
+        fi
     fi
+    unset __conda_setup
+    # <<< conda initialize <<<
 fi
-unset __conda_setup
-# <<< conda initialize <<<
-
--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/home/yixinli/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/home/yixinli/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/home/yixinli/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/home/yixinli/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/yixinli/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+local package_path_str = "/Users/yixinliii/.cache/nvim/packer_hererocks/2.1.1710088188/share/lua/5.1/?.lua;/Users/yixinliii/.cache/nvim/packer_hererocks/2.1.1710088188/share/lua/5.1/?/init.lua;/Users/yixinliii/.cache/nvim/packer_hererocks/2.1.1710088188/lib/luarocks/rocks-5.1/?.lua;/Users/yixinliii/.cache/nvim/packer_hererocks/2.1.1710088188/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/Users/yixinliii/.cache/nvim/packer_hererocks/2.1.1710088188/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -76,17 +76,17 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   ["packer.nvim"] = {
     loaded = true,
-    path = "/home/yixinli/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/Users/yixinliii/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/home/yixinli/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    path = "/Users/yixinliii/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
   ["popup.nvim"] = {
     loaded = true,
-    path = "/home/yixinli/.local/share/nvim/site/pack/packer/start/popup.nvim",
+    path = "/Users/yixinliii/.local/share/nvim/site/pack/packer/start/popup.nvim",
     url = "https://github.com/nvim-lua/popup.nvim"
   }
 }
